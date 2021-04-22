cd89b3cd2c5b7cb0887425a724719baefa9b62b6
HYH <h2005yh@126.com>
2021-04-22 05:24:36 +0000

    基础布局

--- app/helpers.php
+++ app/helpers.php
@@ -1,4 +1,11 @@
 <?php
-function test_helper() {
-    return 'OK';
+//function test_helper() {
+//    return 'OK';
+//}
+
+
+//app.blade.php主布局文件中，根据不同的路由名称定义不同的样式
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
 }
