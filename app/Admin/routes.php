1fe4a4be56aec04d4c25f100d1bc3e5316442f75
HYH <h2005yh@126.com>
2021-04-27 02:19:41 +0000

    管理后台 - 新增、编辑商品

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,6 +13,12 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->get('users', 'UsersController@index');
     $router->get('products', 'ProductsController@index');
+//    新增商品
+    $router->get('products/create', 'ProductsController@create');
+    $router->post('products', 'ProductsController@store');
+//    编辑商品
+    $router->get('products/{id}/edit', 'ProductsController@edit');
+    $router->put('products/{id}', 'ProductsController@update');
 });
 
 //$router->resource('users', UsersController::class);
