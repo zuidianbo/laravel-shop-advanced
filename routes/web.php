9f839755746c9a365712ef4c3bd5563cfe9a34fc
HYH <h2005yh@126.com>
2021-05-12 07:21:10 +0000

    集成支付宝

--- routes/web.php
+++ routes/web.php
@@ -106,3 +106,10 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 //商品详情;
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
+Route::get('alipay', function() {
+    return app('alipay')->web([
+        'out_trade_no' => time(),
+        'total_amount' => '1',
+        'subject' => 'test subject - 测试',
+    ]);
+});
